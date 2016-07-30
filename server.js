ac98f2b5b5914380e2ab038ac04d7e4b96ef57c3
TheSeTh <seth.pro@gmail.com>
2016-07-30 02:06:04 +0200

    final

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ var multer = require('multer');
 var app = express();
 var port = process.env.PORT || 8080;
 
+app.set("views", __dirname + '/views');
+app.set("view engine", "pug");
+
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage, limits: { fileSize: 4194304}});
+
 app.get('/favicon.ico', function(req,res) {
   res.sendStatus(200);
 });
 
 app.get('/', function(req,res) {
-  res.end('gnu');
+  res.render('index');
+});
+
+app.post('/upload',upload.single('fileup'), function(req,res,next) {
+  if(req.file) {
+    res.end(JSON.stringify(req.file.size));
+  }
 });
 
 app.listen(port);
